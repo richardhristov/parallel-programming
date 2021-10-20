fa87fd27b6bd08693ceee8d1fe62f82e4a49a153
Richard Hristov <richardhristov@gmail.com>
2021-10-20 20:34:32 +0300

    Add Excercise2

---
+++ Excercise2/Excercise2/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Excercise2
+{
+    class Program
+    {
+        private static Store _store;
+
+        private static Random _random = new Random();
+
+        private static readonly List<string> _itemNames = new List<string>()
+        {
+            "cheese",
+            "toothbrush",
+            "toilet paper",
+            "shoes",
+            "underwear",
+            "pants",
+            "shirt",
+            "battery",
+            "computer",
+            "processor",
+            "memory",
+            "gpu",
+            "cpu",
+            "psu",
+            "desk",
+            "guitar",
+            "salt",
+            "scooter",
+            "sdcard",
+            "phone"
+        };
+
+        private static Dictionary<string, int> GetRandomItemsAndAmounts(int numberOfItemsMin, int numberOfItemsMax, int amountMin, int amountMax)
+        {
+            var items = new Dictionary<string, int>();
+            var numberOfItems = _random.Next(numberOfItemsMin, numberOfItemsMax);
+            for (int i = 1; i < numberOfItems; i++)
+            {
+                while(true)
+                {
+                    var item = _itemNames[_random.Next(0, _itemNames.Count - 1)];
+                    if (items.ContainsKey(item))
+                    {
+                        continue;
+                    }
+                    items.Add(item, _random.Next(amountMin, amountMax));
+                    break;
+                }
+            }
+            return items;
+        }
+
+        private static void Restock(object iObject)
+        {
+            var sleepMs = _random.Next(100, 1000);
+            Thread.Sleep(sleepMs);
+            // Restock items
+            var i = (int)iObject;
+            var items = GetRandomItemsAndAmounts(1, _itemNames.Count, 1, 20);
+            _store.IncreaseStocks(items);
+            Console.WriteLine($"Restocked items, thread: {i}");
+        }
+
+        private static void BuyIfAvailable(object iObject)
+        {
+            var sleepMs = _random.Next(100, 1000);
+            Thread.Sleep(sleepMs);
+            // Try buying items
+            var i = (int)iObject;
+            var items = GetRandomItemsAndAmounts(1, _itemNames.Count, 1, 3);
+            var bought = _store.DecreaseStocksIfAvailable(items);
+            if (bought)
+            {
+                Console.WriteLine($"Bought items, thread: {i}");
+            }
+            else
+            {
+                Console.WriteLine($"Could not buy items, thread: {i}");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Creating store");
+            var startingStoreContents = GetRandomItemsAndAmounts(_itemNames.Count, _itemNames.Count, 100, 1000);
+            _store = new Store(startingStoreContents);
+
+            Console.WriteLine("Starting test");
+            var buyersCount = 100;
+            var suppliersCount = 5;
+            var threads = new List<Thread>();
+            for (int i = 0; i < buyersCount; i++)
+            {
+                var thread = new Thread(BuyIfAvailable);
+                thread.Start(i);
+                threads.Add(thread);
+            }
+            for (int i = 0; i < suppliersCount; i++)
+            {
+                var thread = new Thread(Restock);
+                thread.Start(i);
+                threads.Add(thread);
+            }
+            foreach (var thread in threads)
+            {
+                thread.Join();
+            }
+            Console.WriteLine("Program complete");
+        }
+    }
+}
---
+++ Excercise2/Excercise2/Store.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise2
+{
+    class Store
+    {
+        private volatile Dictionary<string, int> _products;
+        private object _lock;
+
+        public Store()
+        {
+            _products = new Dictionary<string, int>();
+            _lock = new object();
+        }
+
+        public Store(Dictionary<string, int> products)
+        {
+            _products = products;
+            _lock = new object();
+        }
+
+        public void IncreaseStocks(Dictionary<string, int> products)
+        {
+            lock (_lock)
+            {
+                foreach (var item in products)
+                {
+                    if (!_products.ContainsKey(item.Key))
+                    {
+                        _products.Add(item.Key, 0);
+                    }
+                    _products[item.Key] += item.Value;
+                }
+            }
+        }
+
+        public bool DecreaseStocksIfAvailable(Dictionary<string, int> products)
+        {
+            lock (_lock)
+            {
+                // Check the stocks for each product first
+                foreach (var item in products)
+                {
+                    if (!_products.ContainsKey(item.Key))
+                    {
+                        return false;
+                    }
+                    if (_products[item.Key] < item.Value)
+                    {
+                        return false;
+                    }
+                }
+                // We have enough products, decrement the amounts
+                foreach (var item in products)
+                {
+                    _products[item.Key] -= item.Value;
+                    if (_products[item.Key] == 0)
+                    {
+                        _products.Remove(item.Key);
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
